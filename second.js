b79bbb791bb6ae6b0b082957f7763bfa832d073c
Aru <aatarbaeva98@gmail.com>
2022-11-27 15:20:30 +0600

    local storage task

--- second.js
+++ second.js
@@ -1,4 +1,24 @@
-let ul = document.querySelector("#ul")
 let list = JSON.parse(localStorage.getItem("list")) || []
 
-list.map(item => ul.innerHTML += `<li>${item}</li>`)
+let ul = document.querySelector("#ul")
+let clearBtn = document.querySelector(".clear")
+let removeBtn = document.querySelectorAll(".remove")
+let li = document.querySelectorAll(".li")
+
+function remove(index){
+    let list = JSON.parse(localStorage.getItem("list")) || []
+    list.splice(index,1)
+    localStorage.setItem("list", JSON.stringify(list)) 
+}
+
+list.map(item => ul.innerHTML += `<li class = "li">${item}</li>`)
+
+clearBtn.addEventListener("click", () => {
+    ul.innerHTML = ""
+    localStorage.clear()
+})
+
+ul.addEventListener("click", (e)=> {
+    console.log(e)
+    e.target.innerHTML = ""
+})
